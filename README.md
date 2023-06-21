f1300a7743050223bacb13ec60d262131178b322
Mark Baggett <mbagget1@utk.edu>
2023-06-20 21:22:01 -0400

    Clean up warnings and add choices.

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 This is a simple script that allows you to generate vtt files using OpenAI's Whisper service from a directory of A/V files.
 
-## Example
+## Examples
 
 ```bash
 python transcribe/transcribe.py -d mp3s
 ```
+
+```bash
+python transcribe/transcribe.py -d mp3s -m large
+```
+
 
--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -1,15 +1,19 @@
 import argparse
+import warnings
+# @Todo: remove this when numba is updated to 0.53.1
+warnings.filterwarnings("ignore", message=".*The 'nopython' keyword.*")
 import os
 from tqdm import tqdm
 import whisper
 from whisper.utils import get_writer
 
 class Transcriber:
-    def __init__(self, directory, output, language_model, language):
+    def __init__(self, directory, output, language_model, language, fp16):
         self.directory = directory
         self.output = output
         self.model = language_model
         self.language = language
+        self.fp16 = fp16
 
     def batch_transcribe(self):
         for dirpath, dirnames, filenames in os.walk(self.directory):
@@ -18,7 +22,7 @@ class Transcriber:
 
     def __transcribe(self, file):
         model = whisper.load_model(self.model)
-        result = model.transcribe(file, language=self.language)
+        result = model.transcribe(file, fp16=self.fp16, language=self.language)
         if not os.path.exists(self.output):
             os.makedirs(self.output)
         writer = get_writer('vtt', self.output)
@@ -29,8 +33,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--directory', help='Specify directory to files.', required=True)
     parser.add_argument('-o', '--output', help='Specify output directory', default='output')
-    parser.add_argument('-m', '--model', help='Specify model', default='base')
+    parser.add_argument('-m', '--model', help='Specify model', default='base',
+                        choices=['tiny', 'base', 'small', 'medium', 'large'])
     parser.add_argument('-l', '--language', help='Specify language', default='English')
+    parser.add_argument('-f', '--fp16', action='store_true', help='Use FP16 instead of FP32')
     args = parser.parse_args()
-    x = Transcriber(args.directory, args.output, args.model, args.language)
+    x = Transcriber(args.directory, args.output, args.model, args.language, args.fp16)
     x.batch_transcribe()
