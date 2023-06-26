86636d6ed6b0c38c603bc6152bcee46414b47222
Mark Baggett <mbagget1@utk.edu>
2023-06-26 13:14:04 -0400

    Add option to ignore.

--- transcribe/transcribe.py
+++ transcribe/transcribe.py
@@ -8,17 +8,31 @@ import whisper
 from whisper.utils import get_writer
 
 class Transcriber:
-    def __init__(self, directory, output, language_model, language, fp16):
+    def __init__(self, directory, output, language_model, language, fp16, ignore_existing=False):
         self.directory = directory
         self.output = output
         self.model = language_model
         self.language = language
         self.fp16 = fp16
+        self.ignore_existing = ignore_existing
+        if self.ignore_existing:
+            self.existing = self.__cache_existing()
+        else:
+            self.existing = []
+
+    def __cache_existing(self):
+        existing = []
+        for dirpath, dirnames, filenames in os.walk(self.output):
+            for filename in filenames:
+                if filename.endswith('.vtt'):
+                    existing.append(os.path.splitext(filename)[0])
+        return existing
 
     def batch_transcribe(self):
         for dirpath, dirnames, filenames in os.walk(self.directory):
             for filename in tqdm(filenames):
-                self.__transcribe(os.path.join(self.directory, filename))
+                if os.path.splitext(filename)[0] not in self.existing:
+                    self.__transcribe(os.path.join(self.directory, filename))
         return
 
     def __transcribe(self, file):
@@ -39,6 +53,7 @@ if __name__ == "__main__":
                         choices=['tiny', 'base', 'small', 'medium', 'large'])
     parser.add_argument('-l', '--language', help='Specify language', default='English')
     parser.add_argument('-f', '--fp16', action='store_true', help='Use FP16 instead of FP32')
+    parser.add_argument('-i', '--ignore_existing', action='store_true', help='Ignore A/V files that already have a transcription')
     args = parser.parse_args()
-    x = Transcriber(args.directory, args.output, args.model, args.language, args.fp16)
+    x = Transcriber(args.directory, args.output, args.model, args.language, args.fp16, args.ignore_existing)
     x.batch_transcribe()
